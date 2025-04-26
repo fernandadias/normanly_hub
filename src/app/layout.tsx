ec77881f9d77f9310939759e2dd9a02eef19ef66
fernandadias <diasfeh@gmail.com>
2025-04-26 06:09:59 +0000

    fix: simplifica CSS para resolver erros de Tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR" suppressHydrationWarning>
+    <html lang="pt-BR">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        {children}
       </body>
     </html>
   )
